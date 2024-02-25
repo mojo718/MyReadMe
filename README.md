849a099c2f32091190e73424a192ea5cc13ffb96
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-02-25 18:34:58 -0500

    Did some Psuedo Coding and testing in Index2.  Added functions to generateMarkdown.JS

---
+++ README.md
@@ -0,0 +1,24 @@
+# README
+
+## Description
+Test run
+
+## Table of Contents
+blah blah blach blah
+
+## Installation
+npm install .. duh
+
+## Usage
+used to create Readmes
+
+## Contributing
+do whatevs
+
+## Tests
+same
+
+## License
+![Full License Badge](link-to-badge)
+
+This application is covered under the Full License.
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
 
+const fs = require('fs');
+const inquirer = require('inquirer')
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -36,10 +39,14 @@ const questions = [
 ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(ReadMe, questions) {
+
+ }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    
+ }
 
 // Function call to initialize app
 init();
