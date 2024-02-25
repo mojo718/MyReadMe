8b769ff6e6bfdaf27e16913632fd3af449f68d01
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-02-25 17:18:34 -0500

    created Const with Questions array

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter description:'
+    },
+    {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Enter Table of Contents:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter Installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter Usage Information:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter Test instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license:',
+        licenseChoice: ['Free' , 'full']
+    },
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
